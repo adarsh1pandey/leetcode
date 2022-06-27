50e7eef0b431f24ff492a64213464a39ff039341
Adarsh Pandey <64495278+adarshpnd71@users.noreply.github.com>
2022-06-27 23:06:01 +0530

    Time: 0 ms (100.00%), Space: 8.4 MB (75.38%) - LeetHub

--- 94-binary-tree-inorder-traversal/94-binary-tree-inorder-traversal.cpp
+++ 94-binary-tree-inorder-traversal/94-binary-tree-inorder-traversal.cpp
@@ -12,15 +12,15 @@
 class Solution {
 public:
     void helper(TreeNode *root, vector<int> &ans)
+    {
+        if (root == NULL)
         {
-            if (root == NULL)
-            {
-                return;
-            }
-            helper(root->left, ans);
-            ans.push_back(root->val);
-            helper(root->right, ans);
+            return;
         }
+        helper(root->left, ans);
+        ans.push_back(root->val);
+        helper(root->right, ans);
+    }
     vector<int> inorderTraversal(TreeNode* root) {
         vector<int> ans;
         helper(root, ans);
