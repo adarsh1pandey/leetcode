7b325c1fd144f825a8ed1d5c49130025cbaab689
Adarsh Pandey <64495278+adarshpnd71@users.noreply.github.com>
2022-08-31 01:44:51 +0530

    Time: 23 ms (63.00%), Space: 20 MB (47.51%) - LeetHub

--- 42-trapping-rain-water/42-trapping-rain-water.cpp
+++ 42-trapping-rain-water/42-trapping-rain-water.cpp
@@ -1,33 +1,31 @@
 class Solution {
 public:
     int trap(vector<int>& height) {
-        int n=height.size();
-    int mxL[n];
-    mxL[0]=height[0];
-    for(int i=1;i<n;i++)
-    {
-        mxL[i]=max(mxL[i-1],height[i]);
-    }
-    
-    int mxR[n];
-    mxR[n-1]=height[n-1];
-    for(int i=n-2;i>=0;i--)
-    {
-        mxR[i]=max(mxR[i+1],height[i]);
-    }
-    
-    int water[n];
-    for(int i=0;i<n;i++)
-    {
-        water[i]=min(mxL[i],mxR[i])-height[i];
-    }
-    
-   int sum=0;
-    for(int i=0;i<n;i++)
-    {
-        sum+=water[i];
-    }
-    
-    return sum;
+        int n = height.size();
+        int mxl[n];
+        mxl[0] = height[0];
+        for (int i = 1; i < n; i++)
+        {
+            mxl[i] = max(mxl[i - 1], height[i]);
+        }
+        int mxr[n];
+        mxr[n - 1] = height[n - 1];
+        
+        for (int i = n - 2; i >= 0; i--)
+        {
+            mxr[i] = max(mxr[i + 1], height[i]);
+        }
+        int width[n];
+        int sum = 0;
+        for (int i = 0; i < n ; i++)
+        {
+            width[i] = min(mxr[i], mxl[i]) - height[i];
+            
+        }
+        for (int i = 0; i < n; i++)
+        {
+            sum += width[i];
+        }
+        return sum;
     }
 };
