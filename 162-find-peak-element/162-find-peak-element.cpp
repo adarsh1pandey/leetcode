524484a09c3d3a258287925aaae4253bbc87210f
Adarsh Pandey <64495278+adarshpnd71@users.noreply.github.com>
2022-09-10 23:56:34 +0530

    Time: 7 ms (53.50%), Space: 8.8 MB (89.72%) - LeetHub

--- 162-find-peak-element/162-find-peak-element.cpp
+++ 162-find-peak-element/162-find-peak-element.cpp
@@ -1,32 +1,40 @@
 class Solution {
 public:
-    int findPeakElement(vector<int>& nums) {
-        int low=0;
-        int high=nums.size()-1;
-        while(low<high){
-            int mid=low+(high-low)/2;
-            if(mid>0 && mid<(nums.size()-1)){
-                if(nums[mid]>nums[mid-1] && nums[mid]>nums[mid+1]){
+    int findPeakElement(vector<int>& arr) {
+         int n = arr.size();
+        if(n == 1){
+            return 0;
+        }
+        int low = 0, high = n - 1;
+        while (low <= high){
+            
+            int mid = low + (high - low)/2;
+            if(mid == 0){
+                if(arr[mid] > arr[mid + 1]){
                     return mid;
-                }else if(nums[mid]<nums[mid-1]){
-                    high=mid-1;
                 }else{
-                    low=mid+1;
+                    low = mid + 1;
                 }
-            }else if(nums[mid]==nums[0]){
-                if(nums[0]>nums[1]){
-                    return 0;
+            
+            }else if(mid == n - 1){
+                if(arr[mid - 1] < arr[mid]){
+                    return mid;    
                 }else{
-                    return 1;
+                    high = mid - 1;
                 }
-            }else if(mid==nums.size()-1){
-                if(nums.size()-1>nums.size()-2){
-                    return nums.size()-1;
+                 
+            }else{
+                if(arr[mid] > arr[mid - 1] && arr[mid] > arr[mid + 1]){
+                    return mid;
+                }
+                else if(arr[mid] < arr[mid + 1]){
+                    low = mid + 1;
                 }else{
-                    return nums.size()-2;
+                    high = mid - 1;
                 }
             }
         }
-        return high;
+        
+        return -1;
     }
 };
