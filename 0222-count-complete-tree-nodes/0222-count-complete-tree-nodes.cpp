8335cac6e2db94cb2f3e5fe6ea055c608ea41c2d
Adarsh Pandey <64495278+adarshpnd71@users.noreply.github.com>
2022-11-03 19:57:15 +0530

    Time: 83 ms (7.27%), Space: 31.6 MB (6.60%) - LeetHub

--- 0222-count-complete-tree-nodes/0222-count-complete-tree-nodes.cpp
+++ 0222-count-complete-tree-nodes/0222-count-complete-tree-nodes.cpp
@@ -14,6 +14,24 @@ public:
     int countNodes(TreeNode* root) {
         if (root == NULL)
             return 0;
-        return 1 + countNodes(root->left) + countNodes(root->right);
+        int count = 0;
+        queue<TreeNode*> q;
+        q.push(root);
+        while (! q.empty())
+        {
+            int qSize = q.size();
+            
+            for (int i = 0; i < qSize; i++)
+            {
+                TreeNode* curr = q.front();
+                q.pop();
+                count++;
+                if (curr->left)
+                    q.push(curr->left);
+                if (curr->right)
+                    q.push(curr->right);
+            }
+        }
+        return count;
     }
 };
