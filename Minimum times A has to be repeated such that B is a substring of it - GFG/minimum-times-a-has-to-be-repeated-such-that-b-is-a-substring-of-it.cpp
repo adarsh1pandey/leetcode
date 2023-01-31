1a4af569262446e77d5e022b11782e25800c894d
Adarsh Pandey <64495278+adarsh1pandey@users.noreply.github.com>
2023-01-31 08:34:29 +0530

    Added solution - LeetHub

--- Minimum times A has to be repeated such that B is a substring of it - GFG/minimum-times-a-has-to-be-repeated-such-that-b-is-a-substring-of-it.cpp
+++ Minimum times A has to be repeated such that B is a substring of it - GFG/minimum-times-a-has-to-be-repeated-such-that-b-is-a-substring-of-it.cpp
@@ -12,18 +12,22 @@ class Solution {
         // code here
         string s = A;
         int moves = 1;
+        // check if size of A is less than B
         while (A.size() < B.size()){
             A += s;
             moves++;
         }
+        // find function is used to find whether B is available in A or not 
         if (A.find(B) != -1){
             return moves;
         }
+        // Now we add one more time to be safe 
         A += s;
         moves++;
         if (A.find(B) != -1){
             return moves;
         }
+        // if we don't find then return -1
         return -1;
     }
 };
